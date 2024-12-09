f916dd8fbb17693aec99c30547a055f912c93848
maria <mariamibrahimmi4142726@gmail.com>
2024-12-09 13:06:12 +0200

    first commit

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color basicWhite = Colors.white;
+  static const Color greentit = Color(0xff194338);
+  static const Color greensubtit = Color(0xff1C4A3E);
+  static const Color grey = Color(0xffD9D9D9);
+  static const Color yellow = Color(0xffB98F05);
+  static const Color secondgreenButt = Color(0xffDEE9E4);
+  static const Color greenBorder = Color(0xff6F9287);
+}
