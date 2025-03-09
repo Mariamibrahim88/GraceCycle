8124051d8668a5f5e540723e2029b2231c71d1d1
shahdashraf2003 <shahdashraf396@gmail.com>
2025-03-09 23:03:44 +0200

    add items in see all view

--- lib/features/home/presentation/views/see_all_view.dart
+++ lib/features/home/presentation/views/see_all_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grace_cycle/core/utils/app_colors.dart';
 import 'package:grace_cycle/features/home/presentation/views/widgets/see_all_view_body.dart';
 
 class SeeAllView extends StatelessWidget {
@@ -6,6 +7,8 @@ class SeeAllView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SeeAllViewBody();
+    return const SafeArea(
+        child: Scaffold(
+            backgroundColor: AppColors.basicWhite, body: SeeAllViewBody()));
   }
 }
--- lib/features/home/presentation/views/widgets/header_of_vendor_name.dart
+++ lib/features/home/presentation/views/widgets/header_of_vendor_name.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:grace_cycle/core/routes/app_routes.dart';
+import 'package:grace_cycle/core/utils/app_navigate.dart';
 import 'package:grace_cycle/features/Authentication/presentation/views/widgets/custom_yellow_text.dart';
 
 class HeaderOfVendorName extends StatelessWidget {
@@ -23,8 +25,11 @@ class HeaderOfVendorName extends StatelessWidget {
               color: Colors.black,
             ),
           ),
-          const CustomYellowText(
+           CustomYellowText(
             text2: 'See all',
+            onPressed: () {
+              navigate(context: context, route: Routes.seeAll);
+            },
             fontsize: 18,
             fontWeight: FontWeight.w700,
           )
