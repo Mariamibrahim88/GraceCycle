d341b80eb669e32b80add9b4365d45ba1df324a4
shahdashraf2003 <shahdashraf396@gmail.com>
2025-03-10 18:35:59 +0200

    changes

--- lib/features/home/presentation/views/widgets/see_all_view_body.dart
+++ lib/features/home/presentation/views/widgets/see_all_view_body.dart
@@ -3,7 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:grace_cycle/core/utils/app_assets.dart';
 import 'package:grace_cycle/core/utils/app_spacing.dart';
 import 'package:grace_cycle/core/widgets/custom_app_bar.dart';
-import 'package:grace_cycle/features/home/presentation/views/widgets/custom_list_of_food_ver.dart';
+import 'package:grace_cycle/features/home/presentation/views/widgets/custom_list_of_categorized_food.dart';
 import 'package:grace_cycle/features/home/presentation/views/widgets/custom_list_of_vendor_ver.dart';
 
 class SeeAllViewBody extends StatelessWidget {
@@ -13,25 +13,7 @@ class SeeAllViewBody extends StatelessWidget {
   final bool isVendor;
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(slivers: [
-      const CustomHomeAppBar(),
-      SliverToBoxAdapter(
-        child: CustomAppBar(
-          title: 'Main Dashes',
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      SliverToBoxAdapter(
-        child: CustomListOfVendorVer(
-            foodTitle: 'Main Dashes', foodAsset: AppAssets.findYourDish),
-      ),
-      SliverToBoxAdapter(
-          // child: CustomListOfFoodVer(
-          //     foodTitle: 'Main Dashes', foodAsset: AppAssets.findYourDish),
-          ),
-    ]);
+    
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 30.h),
       child: CustomScrollView(slivers: [
@@ -52,11 +34,9 @@ class SeeAllViewBody extends StatelessWidget {
                     foodTitle: 'Main Dashes',
                     foodAsset: AppAssets.findYourDish),
               )
-            : SliverToBoxAdapter(
-                child: CustomListOfFoodVer(
-                    foodTitle: 'Main Dashes',
-                    foodAsset: AppAssets.findYourDish),
-              ),
+            : const SliverToBoxAdapter(
+                child: ListOfCategorizedFood()
+            )
       ]),
     );
   }
