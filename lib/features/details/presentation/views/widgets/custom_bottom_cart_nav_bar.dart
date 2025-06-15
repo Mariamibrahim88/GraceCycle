1a2cbc48de8bebe4ee115ecd5115294254e240b4
maria <mariamibrahimmi4142726@gmail.com>
2025-06-16 00:42:14 +0300

    update total price of food item

--- lib/features/details/presentation/views/widgets/custom_bottom_cart_nav_bar.dart
+++ lib/features/details/presentation/views/widgets/custom_bottom_cart_nav_bar.dart
@@ -1,45 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:grace_cycle/core/utils/app_colors.dart';
 import 'package:grace_cycle/core/utils/app_text_styles.dart';
+import 'package:grace_cycle/features/details/presentation/manager/cubit/details_cubit.dart';
 
 class CustomBottomCartNavBar extends StatelessWidget {
   const CustomBottomCartNavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 90.h,
-      child: ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-          backgroundColor: AppColors.basicWhite,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.zero,
-          ),
-        ),
-        child: Container(
-            height: 46.h,
-            width: 344.w,
-            decoration: BoxDecoration(
-              color: AppColors.greenButt,
-              borderRadius: BorderRadius.circular(16.r),
-            ),
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10.w),
-              child: Row(
-                children: [
-                  Text(
-                    'Add to cart',
-                    style: AppTextStyles.nunito600Size16White,
-                  ),
-                  const Spacer(),
-                  Text('EGP 20', style: AppTextStyles.nunito600Size16White),
-                ],
+    return BlocBuilder<DetailsCubit, DetailsState>(
+      builder: (context, state) {
+        final cubit = context.read<DetailsCubit>();
+        final quantity = cubit.quantity;
+        final pricePerUnit = cubit.foodItemDetails?.newPrice ?? 0;
+        final totalPrice = quantity * pricePerUnit;
+
+        return SizedBox(
+          width: double.infinity,
+          height: 90.h,
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.basicWhite,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.zero,
               ),
-            )),
-      ),
+            ),
+            child: Container(
+              height: 46.h,
+              width: 344.w,
+              decoration: BoxDecoration(
+                color: AppColors.greenButt,
+                borderRadius: BorderRadius.circular(16.r),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.w),
+                child: Row(
+                  children: [
+                    Text(
+                      'Add to cart',
+                      style: AppTextStyles.nunito600Size16White,
+                    ),
+                    const Spacer(),
+                    Text(
+                      'EGP ${totalPrice.toStringAsFixed(2)}',
+                      style: AppTextStyles.nunito600Size16White,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/features/details/presentation/views/widgets/custom_list_of_similar_food_item.dart
+++ lib/features/details/presentation/views/widgets/custom_list_of_similar_food_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:grace_cycle/core/routes/app_routes.dart';
+import 'package:grace_cycle/core/utils/app_navigate.dart';
 import 'package:grace_cycle/features/details/data/models/food_item_details_model.dart';
 import 'package:grace_cycle/features/home/presentation/views/widgets/food_card.dart';
 
@@ -19,8 +21,16 @@ class CustomListOfSimilarFoodItem extends StatelessWidget {
           itemBuilder: (context, index) {
             return Padding(
               padding: EdgeInsets.only(left: 8.w),
-              child: FoodCard(
-                foodItemModel: foodItemDetailsModel.similarItems[index],
+              child: GestureDetector(
+                onTap: () {
+                  navigate(
+                      context: context,
+                      route: Routes.foodDetails,
+                      arg: foodItemDetailsModel.similarItems[index].id);
+                },
+                child: FoodCard(
+                  foodItemModel: foodItemDetailsModel.similarItems[index],
+                ),
               ),
             );
           }),
